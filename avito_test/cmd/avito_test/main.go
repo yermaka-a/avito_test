62d465bc5911c22cf5b313f769c7daeb9924e526
yermaka <ermak.080@mail.ru>
2025-11-23 22:32:05 +0800

    add server

--- avito_test/cmd/avito_test/main.go
+++ avito_test/cmd/avito_test/main.go
@@ -1,5 +1,78 @@
 package main
 
+import (
+	"avito_test/internal/app"
+	"avito_test/internal/config"
+	"avito_test/internal/logger"
+	"avito_test/internal/storage/postgres"
+	"context"
+	"fmt"
+	"os"
+)
+
 func main() {
-	return
+	config := config.MustLoad()
+	log := logger.Setup(os.Stdout, config.LogLevel())
+
+	ctx := context.Background()
+
+	dbc := config.DBConfig
+	storagePath := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbc.USER, dbc.PASS, dbc.HOST, dbc.PORT, dbc.DBName)
+	storage, err := postgres.New(ctx, storagePath, log)
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer storage.Close()
+	app := app.New(*config, log, storage)
+
+	err = app.Run()
+
+	if err != nil {
+		log.Warn("server running error", err.Error())
+	}
+	// err = storage.CreateTeam(ctx, &models.Team{
+	// 	TeamName: "MyTeam1",
+	// 	Members: []*models.TeamMember{
+	// 		{
+	// 			UserID:   "1",
+	// 			Username: "Thomas",
+	// 			IsActive: true,
+	// 		},
+	// 		{
+	// 			UserID:   "2",
+	// 			Username: "John",
+	// 			IsActive: false,
+	// 		},
+	// 	},
+	// })
+	// fmt.Print("%s", err)
+	// team, err := storage.GetTeamWithMembers(ctx, "MyTeam1")
+	// teamJson, err := json.MarshalIndent(team, "-", "  ")
+	// fmt.Printf("%v, %v", string(teamJson), err)
+
+	// reviewers, err := storage.CreatePR(ctx, models.NewPullRequest("154", "my first pr", "2"))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// revJSON, _ := json.MarshalIndent(reviewers, "-", "  ")
+	// fmt.Printf("%v", string(revJSON))
+
+	// pr, err := storage.PRMarkAsMerged(ctx, "154")
+	// if err != nil {
+	// 	fmt.Print(err)
+	// }
+	// b, _ := json.MarshalIndent(pr, "--", "  ")
+	// fmt.Printf("%v", string(b))
+
+	// pr, err := storage.Reassign(ctx, "154", "1")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// b, _ := json.MarshalIndent(pr, "--", "  ")
+	// fmt.Printf("%v", string(b))
+	// user, err := storage.GetReview(ctx, "3")
+	// b, _ := json.MarshalIndent(user, "--", "  ")
+	// fmt.Printf("%v,  %v", string(b), err)
+	fmt.Println("Успешное подключение к PostgreSQL через пул соединений!")
+
 }
---
+++ avito_test/internal/app/app.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"avito_test/internal/config"
+	"avito_test/internal/http"
+	"avito_test/internal/http/handlers"
+	"avito_test/internal/services"
+	"avito_test/internal/storage/postgres"
+	"fmt"
+	"log/slog"
+)
+
+type App struct {
+	cfg    config.Config
+	logger *slog.Logger
+	server *http.Server
+}
+
+func New(cfg config.Config, logger *slog.Logger, storage *postgres.Storage) *App {
+
+	service := services.New(logger, storage, storage, storage)
+	handler := handlers.New(logger, service)
+	server := http.NewServer(handler)
+
+	return &App{
+		cfg:    cfg,
+		logger: logger,
+		server: server,
+	}
+}
+
+func (a *App) Run() error {
+	a.logger.Info(fmt.Sprintf("starting server on port %s", a.cfg.Port()))
+	return a.server.Run(fmt.Sprintf(":%s", a.cfg.Port()))
+}
